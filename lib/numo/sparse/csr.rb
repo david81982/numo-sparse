4afee727d0ed372fc362c836182e27c2319d7a8d
David <davidgomez81982@gmail.com>
2019-07-05 17:11:59 +0900

    Passing csr and csr test

--- lib/numo/sparse/csr.rb
+++ lib/numo/sparse/csr.rb
@@ -3,6 +3,8 @@ require 'numo/sparse/base'
 module Numo
   module Sparse
     class CSR < BaseTensor
+      attr_reader :shape, :dtype, :data, :indptr, :indices, :blank, :results
+	  
       def self.max_ndim
         2
       end
@@ -15,7 +17,10 @@ module Numo
         @shape = check_shape(narray.shape).dup.freeze
         @dtype = narray.class
         @data = narray[narray.ne(0)]
-		#index creation
+        #to_csr #####
+		blank = narray.ne(0) #####
+p blank
+		@results = make_results(narray) #####
       end
 
       private def initialize_empty(shape, dtype)
@@ -23,54 +28,57 @@ module Numo
         @dtype = check_dtype(dtype)
         @data = []
       end
-	  
-	  #draft of the conversion of regular matrix to csr
-	  private def to_csr(rows, columns,  data)
+      
+      private def make_results(blank) #####
 		#initialization
-		i, j, matrix = rows, columns, data
-		a, b = 0, 0
-		data = []
-		indptr = []
-		count = []
-		count.at[i] = 0
-		i += 1
-		
-		#start of the code itself
-		while a < 2
-			while b < 2
-				if (matrix.at[i][j] != 0)
-					count += 1
-					data.push=(matrix.at[i][j])
-					indices.push(j)
-				end
-			end
-			indptr.push(count) 
-		end
-	  end
-	  
-	  
-	  #begin This is a draft for converting a csr matrix back to normal
-	  
-	  #private def to_regular_matrix()
-		#t, a, c, d, tempor, i = 0, 0, 0, 0, 0, 1	
-		
-		#while t < (indptr.length() - 1)	 Create a limit for the rows
-			#tempor = (indptr.at[i] - indptr.at[i-1] 	number of non-zero values in the row
-			#while a < tempor 	#assign the data to their positions
-				#matrix.at[t][indices.at[c]] = data.at[d] 	find the correct position and input the correct values
-				#TODO: correct the matrix.at[]
-				#c += 1
-				#d += 1
-				#a += 1
-			#end
-			#i += 1
-			#t += 1
-			#a = 0
-		#end
-	  #end
-	  
-	  #end
-	  
+        i, j, matrix = shape[0], shape[1], blank
+        a, count = 0, 0
+        indices = []
+        indptr = []
+        indptr[0] = 0
+p blank
+        #start of the code itself
+        while a < i
+		  b = 0
+		  
+          while b < j
+            if (matrix[a,b] != 0)
+              count += 1
+              indices.push(b)
+            end
+			b += 1
+          end
+		  
+          indptr.push(count) 
+		  a += 1		  
+        end
+p count
+p data
+p indices
+p indptr
+		[data, indices, indptr]
+      end
+      
+      
+      #begin This is a draft for converting a csr matrix back to normal
+      #private def to_regular_matrix()
+        #t, a, c, d, tempor, i = 0, 0, 0, 0, 0, 1	
+        
+        #while t < (indptr.length() - 1)	 Create a limit for the rows
+          #tempor = (indptr[i] - indptr[i-1] 	number of non-zero values in the row
+          #while a < tempor 	#assign the data to their positions
+              #matrix[t][indices[c]] = data[d] 	find the correct position and input the correct values
+              #TODO: correct the matrix[]
+              #c += 1
+              #d += 1
+              #a += 1
+          #end
+          #i += 1
+          #t += 1
+		  #a = 0
+      #end
+    #end
+      
     end
   end
 end
