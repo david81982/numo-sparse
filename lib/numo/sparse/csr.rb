fdebbbbfb7d181c7d517e87554ce3a1a02c70869
David <davidgomez81982@gmail.com>
2019-07-14 11:15:32 +0900

    to_narray added

--- lib/numo/sparse/csr.rb
+++ lib/numo/sparse/csr.rb
@@ -3,6 +3,8 @@ require 'numo/sparse/base'
 module Numo
   module Sparse
     class CSR < BaseTensor
+      attr_reader :shape, :dtype, :data, :indptr, :indices
+
       def self.max_ndim
         2
       end
@@ -15,13 +17,54 @@ module Numo
         @shape = check_shape(narray.shape).dup.freeze
         @dtype = narray.class
         @data = narray[narray.ne(0)]
-        # TODO: index creation
+        make_csr(narray)
       end
 
       private def initialize_empty(shape, dtype)
         @shape = check_shape(shape).dup.freeze
         @dtype = check_dtype(dtype)
         @data = []
+      end
+
+      private def make_csr(narray)
+        row_limit, col_limit = shape[0], shape[1]
+        matrix, curr_row, count = narray, 0, 0
+        indices = []
+        indptr = []
+        indptr[0] = 0
+        while curr_row < row_limit
+          curr_col = 0
+          while curr_col < col_limit
+            if matrix[curr_row,curr_col] != 0
+              count += 1
+              indices.push(curr_col)
+            end
+            curr_col += 1
+          end
+          indptr.push(count) 
+          curr_row += 1
+        end
+        @indices = Numo::Int32[*indices]
+        @indptr = Numo::Int32[*indptr]
+      end
+
+      def to_narray()
+        narray = data.class.zeros(shape)
+        row, row_lim, curr_ind = 0, 0, 0
+        curr_data, current, curr_ptr = 0, 0, 1
+        while row < (indptr.size - 1)
+          row_lim = (indptr[curr_ptr] - indptr[curr_ptr-1])
+          while current < row_lim
+            narray[row, indices[curr_ind]] = data[curr_data]
+            curr_ind += 1
+            curr_data += 1
+            current += 1
+          end
+          curr_ptr += 1
+          row += 1
+          current = 0
+        end
+        narray
       end
     end
   end
