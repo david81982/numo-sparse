df42f72c69621caa022466a8acc2f71c23a5956b
David <davidgomez81982@gmail.com>
2019-07-22 19:57:57 +0900

    added the get row function in base

--- lib/numo/sparse/base.rb
+++ lib/numo/sparse/base.rb
@@ -37,6 +37,19 @@ module Numo
         return dtype if dtype < Numo::NArray
         raise ArgumentError, "Invalid dtype: #{dtype}"
       end
+
+      def get_row(row)
+        max_col, curr_col, row = shape[1], 0, row
+        matrix = self.class.new(data, indices, indptr, shape).to_narray
+        result = data.class.zeros([1, max_col])
+        until curr_col == max_col do
+          if matrix[row, curr_col] != 0
+            result[0, curr_col] = matrix[row, curr_col]
+          end
+          curr_col += 1
+        end
+        self.class.new(result)
+      end
     end
   end
 end
